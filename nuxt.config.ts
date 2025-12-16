d0feb4e145afda53a8e18ffddf0b903121258c66
Gabriel Xavier <k2.gabriel@tigre.com>
2025-12-16 17:22:33 -0300

    Implementacao favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,13 @@ export default defineNuxtConfig({
       ],
       htmlAttrs: {
         lang: 'pt-BR'
-      }
+      },
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ]
     }
   },
   compatibilityDate: '2025-07-15',
